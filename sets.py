46aa98d043ed6b137dda29daf09c6637ce72ee6d
Nataly Polishchuk <romanovnata@gmail.com>
2024-03-27 23:19:45 +0100

    sets and loop

--- sets.py
+++ sets.py
@@ -1,8 +1,15 @@
 def remove_duplicates(ls):
     ls = set(ls)
     return(list(ls))
-    
+
+def remove_dupl_loop(ls):
+    result = []
+    for e in ls:
+        if e not in result:
+            result.append(e)     
+    return(result)
+
 ls = [1,2,2,2,3,3,4,6]
 print(ls)
-ls = remove_duplicates(ls)
-print(ls)
+print(remove_duplicates(ls))
+print(remove_dupl_loop(ls))
