bded438377fedc3f07e02e59bec6352f6bc17540
Nataly Polishchuk <romanovnata@gmail.com>
2024-04-01 23:21:21 +0200

    reverse

--- Fibonnaci_numbers.py
+++ Fibonnaci_numbers.py
@@ -1,7 +1,7 @@
 n = int(input("how many Fibonnaci numbers to generate: "))
 
 fib = []
-for i in range(0,n):
+for i in range(0, n):
     if i < 2 :
         fib.append(1)
     else:
---
+++ reverse_word_order.py
@@ -0,0 +1,6 @@
+def reverse(a):
+    return(a[::-1])
+
+result = (input ("Enter the long phrase:")).split()
+result = reverse(result)
+print(result)
